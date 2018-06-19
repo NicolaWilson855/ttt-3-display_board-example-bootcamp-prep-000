d0501f5cd3b35725aa3ed8226e82aa920e00fe80
nicola.wilson855@gmail.com <nicola.wilson855@gmail.com>
2018-06-19 21:13:38 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,8 @@
 def display_board
   puts first_row= ["   ", "   ", "   " ]
-  puts second_row= 
+  puts second_row= ["   ", "   ", "   " ]
+  puts third_row= ["   ", "   ", "   " ]
+  
 end
 display_board
 
